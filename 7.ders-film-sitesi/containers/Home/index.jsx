b74ffaf54e9f385507e4f40508fe5eacc7319b21
zafergunayonalan <zafergunay7@gmail.com>
2024-06-13 13:42:59 +0300

    movie-sections

--- 7.ders-film-sitesi/containers/Home/index.jsx
+++ 7.ders-film-sitesi/containers/Home/index.jsx
@@ -5,6 +5,7 @@ import Genres from '@/mocks/genres.json'
 
 import FeaturedMovie from '@/components/featured-movie'
 import Categories from '@/components/categories'
+import MoviesSection from '@/components/movies-section'
 
 
 const HomeContainer = () => {
@@ -12,6 +13,14 @@ const HomeContainer = () => {
     <div>
         <FeaturedMovie movie={Movies.results[0]}/>
         <Categories categories={Genres.genres.slice(0,5)}/>
+        <MoviesSection 
+          title="Populer Films" 
+          movies={Movies.results.slice(1,7)}
+        />
+        <MoviesSection 
+          title="Your Favorites" 
+          movies={Movies.results.slice(7,13)}
+        />
     </div>
   )
 }
